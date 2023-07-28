96ffd684430f856dd926b6c7f0c0850e621edabe
Madhuri Battula <137879232+BattulaMadhuri@users.noreply.github.com>
2023-07-28 20:20:16 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,29 @@
+ReactDOM.render(
+    <div>
+        <h1>Arnold Schwarzenegger</h1>
+        <img src="https://i.guim.co.uk/img/media/3febfd354b256657638d299f7fc417ff19dc5418/291_156_1642_985/master/1642.jpg?width=465&dpr=1&s=none"/>
+        <h3>About Him</h3>
+        <p>He was born on July 30, 1947 in Thal, Austria.</p>
+        <p>Arnold is an Austrian and American actor.</p>
+        <h3>Full Name</h3>
+        <p>ARNOLD ALOIS SCHWARZENEGGER</p>
+        <h3>Parents</h3>
+        <p>Gustav Schwarzenegger.</p>
+        <h2>Early Life and Carrer</h2>
+        <img src="https://image-cdn.essentiallysports.com/wp-content/uploads/WhatsApp-Image-2022-12-06-at-8.28.33-AM.jpeg"/>
+        <p>He is retired professional bodybuilder.</p>
+        <p>And Chairman of the President's Council on Physical Fitness and Sports</p>
+        <p>Schwarzenegger began lifting weights at age 15.</p>
+        <p>And won the Mr. Universe title at age 20, and the Mr. Olympia title seven times.</p>
+        <h2>Success</h2>
+        <img src="https://www.goldderby.com/wp-content/uploads/2019/07/Commando.jpg?w=620"/>
+        <p>He gained worldwide fame as a Hollywood action star, and he has roles in high-profile action movies.</p>
+        <p>He is the founder of the film production company Oak Productions.</p>
+        <p>He served as the 38th governor of California from 2003 to 2011.</p>
+        <p>Schwarzenegger served in the Austrian Army in 1965.</p>
+        <h3>Best Win</h3>
+        <p>IFBB Mr. Olympia, Seven Times.</p>
+        <h3>Profession</h3>
+        <p>Bodybuilder, Actor, BusinessMan.</p>
+    </div>
+,document.getElementById("content"));
